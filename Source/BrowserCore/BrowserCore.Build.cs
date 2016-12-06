f432e023d7d52d2402d48ee6820f01cf80ffd9e3
flufy3d <flufy3d@gmail.com>
2016-12-06 18:14:57 +0800

    fix Official  Engine Build Error
    In Official binary release version .
    the AddEngineThirdPartyPrivateStaticDependencies
    seems not work.
    so need to add it to PrivateDependencyModuleNames

--- Source/BrowserCore/BrowserCore.Build.cs
+++ Source/BrowserCore/BrowserCore.Build.cs
@@ -21,7 +21,8 @@ public class BrowserCore : ModuleRules
 				"SlateCore",
 				"Serialization",
 				"CEF3Utils",
-			}
+                "CEF3"
+            }
 		);
 
 		if (Target.Platform == UnrealTargetPlatform.Win64
